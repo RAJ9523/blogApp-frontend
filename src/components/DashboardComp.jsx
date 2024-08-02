646911e00eb60996a29ab14eaf34dbf8e6648f16
Ronit Raj <96584307+RAJ9523@users.noreply.github.com>
2024-08-02 08:52:02 +0530

    Update DashboardComp.jsx
    commit-5

--- src/components/DashboardComp.jsx
+++ src/components/DashboardComp.jsx
@@ -23,7 +23,8 @@ export default function DashboardComp() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/getusers?limit=5`);
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/getusers?limit=5`,
+                                credentials: 'include');
         const data = await res.json();
         if (res.ok) {
           setUsers(data.users);
@@ -36,7 +37,8 @@ export default function DashboardComp() {
     };
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/post/getposts?limit=5`);
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/post/getposts?limit=5`,
+                                credentials: 'include');
         const data = await res.json();
         if (res.ok) {
           setPosts(data.posts);
@@ -49,7 +51,8 @@ export default function DashboardComp() {
     };
     const fetchComments = async () => {
       try {
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/comment/getcomments?limit=5`);
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/comment/getcomments?limit=5`,
+                                credentials: 'include');
         const data = await res.json();
         if (res.ok) {
           setComments(data.comments);
