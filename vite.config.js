9dc799b89a3ed3a50711493f62a6bd007c08de19
Ronit Raj <96584307+RAJ9523@users.noreply.github.com>
2024-08-02 08:26:56 +0530

    Update vite.config.js
    updatede config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://blogapp-api-811v.onrender.com',
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
