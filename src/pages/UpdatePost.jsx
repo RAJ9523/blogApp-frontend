9e8cb71c24b71934bcfd106ae8075825004e2111
Ronit Raj <96584307+RAJ9523@users.noreply.github.com>
2024-08-02 18:05:24 +0530

    Update UpdatePost.jsx
    upatdesearch

--- src/pages/UpdatePost.jsx
+++ src/pages/UpdatePost.jsx
@@ -7,7 +7,7 @@ import {
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
-import { app } from '../../firebase.js';
+import { app } from '../redux/firebase.js';
 import { useEffect, useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
